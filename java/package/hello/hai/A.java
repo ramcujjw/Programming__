d4d548f7ee584ff501a5aed6e806c8a907166577
ramcujjw <ramcy.r77@gmail.com>
2025-11-12 15:30:12 +0530

    package

---
+++ java/package/hello/hai/A.java
@@ -0,0 +1,7 @@
+package hello.hai;
+
+public class A {
+void display() {
+	 System.out.println("Display from class A");
+}
+}
---
+++ java/package/hello/hai/hey.java
@@ -0,0 +1,13 @@
+package hello.hai;
+import  hello.hai.A;
+public class hey {
+	
+	public hey(){
+		
+		A a =new A();
+		 a.display();	
+	}
+	 public static void main(String[] args) {
+		new hey();
+	 }
+}
